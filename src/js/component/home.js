c7845724145df9edc752c0d534fc1fc6652879ff
GuillermoCairo26 <guillermocairo14@gmail.com>
2021-05-06 14:28:29 +0000

    Parte visual sin funcionalidad

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,49 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		// Contenedor del to-do list
+		<div className="container">
+			{/* Titulo del to-do list */}
+			<h1 className="titulo">To-do list</h1>
+			<ul className="list-group">
+				{/* Form agregar tarea */}
+				<li className="list-group-item">
+					<form>
+						<div className="form-row align-items-center">
+							<div className="col-11">
+								<label
+									className="sr-only"
+									for="inlineFormInput">
+									Name
+								</label>
+								<input
+									type="text"
+									class="form-control mb-2"
+									id="inlineFormInput"
+									placeholder="Nueva tarea"></input>{" "}
+							</div>
+							<div className="col-1">
+								<button
+									type="submit"
+									className="btn btn-primary mb-2">
+									⬇️
+								</button>
+							</div>
+						</div>
+					</form>
+
+					<li className="list-group-item">
+						A second item{" "}
+						<button className="borrar.tarea">X</button>
+					</li>
+					<li className="list-group-item">A third item</li>
+					<li className="list-group-item">A fourth item</li>
+					<li className="list-group-item">And a fifth one</li>
+				</li>
+				<footer>. </footer>
+			</ul>
 		</div>
 	);
 }
